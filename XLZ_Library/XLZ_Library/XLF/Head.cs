13eed5a5a31a6bdfd00afe4116838a54bcf2657e
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-10-23 09:46:42 +0200

    Adding a new structure of Xlf classes

---
+++ XLZ_Library/XLZ_Library/XLF/Head.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+using XLZ_Library.XLF.TransUnit;
+
+namespace XLZ_Library.XLF
+{
+    public class Head
+    {
+    }
+}
--- XLZ_Library/XLZ_Library/Xlf.cs
+++ XLZ_Library/XLZ_Library/Xlf.cs
@@ -29,7 +29,21 @@ using XLZ_Library.XLF.TransUnit;
  *			- <it> (to be described).
  *			- <ph> (to be described).
  *			
- *	Class for modelling Xlf file should 		
+ *	Class for modelling Xlf file should:
+ *	1) Contain XmlDocument field to store the actual XML document which is parsed in this class. This is done by the field of the name "xlfDocument". 
+ *	2) Contain three fields modeling three main parts of Xlf structure:
+ *	 2.1.) XmlNode metaInformation;
+ *	 2.2.) XmlNode header;
+ *	 2.3.) XmlNode body;
+ *	3) 
+ *	
+ *	Class for modelling Xlf file therefore should permit to:
+ *	1) Create and call 
+ *	
+ *	
+ *	Class for modelling Xlf file should not permint to:
+ *	1) Get nor set of based XmlDocument. 
+ *	2) 
  */
 
 namespace XLZ_Library
@@ -41,6 +55,7 @@ namespace XLZ_Library
 
 		public XmlDocument xlfDocument;
 
+		public XmlNode meta;
 		public XmlNode head;
 		public XmlNode body;
 
@@ -139,7 +154,8 @@ namespace XLZ_Library
 
 			xlfDocument = new XmlDocument();
 			xlfDocument.Load(inputFile);
-			
+
+			meta = xlfDocument.SelectSingleNode("//xliff");
 			head = xlfDocument.SelectSingleNode("//header");
 			body = xlfDocument.SelectSingleNode("//body");
 
