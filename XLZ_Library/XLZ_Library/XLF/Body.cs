a865e7b2f3964b009898f8fa2200fd343bf0982c
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-10-23 10:41:29 +0200

    Changing the way of creating Trans Unit nodes

--- XLZ_Library/XLZ_Library/XLF/Body.cs
+++ XLZ_Library/XLZ_Library/XLF/Body.cs
@@ -12,9 +12,40 @@ using System.Xml.Linq;
 using System.Runtime.CompilerServices;
 using XLZ_Library.XLF.TransUnit;
 
-namespace XLZ_Library.XLF
+namespace XLZ_Library
 {
     public class Body
     {
-    }
+
+        /* Fields */
+
+        public XmlNode xmlBody;
+
+        public XmlNodeList xmlTransUnitList;
+        public List<TransUnit> transUnitList;
+
+		/* Properties */
+
+
+		/* Methods */
+
+		/* Constructors */
+
+		public Body(XmlNode xmlBody)
+		{
+			/* Here should be added validation of XML file. */
+			this.xmlBody = xmlBody;
+				
+			xmlTransUnitList = xmlBody.SelectNodes("//trans-unit");
+			transUnitList = new List<TransUnit>();
+
+			TransUnit auxiliaryTransUnit = new TransUnit();
+			foreach (XmlNode xmlNode in xmlTransUnitList)
+			{
+				auxiliaryTransUnit = new TransUnit(xmlNode);
+				transUnitList.Add(auxiliaryTransUnit);
+			}
+
+		}
+	}
 }
