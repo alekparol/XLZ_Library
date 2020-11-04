5c5c9db709c1871f7170aae4a0c0df6ebcc92a62
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-11-04 16:09:09 +0100

    Adding Bpt Documentation

--- XLZ_Library/XLZ_Library/XLF/TransUnit/Languages/Elements/Bpt.cs
+++ XLZ_Library/XLZ_Library/XLF/TransUnit/Languages/Elements/Bpt.cs
@@ -50,17 +50,21 @@ using XLZ_Library.XLF;
  * 1.) XmlNode xmlBptNode field to store <bpt> XmlNode with its XmlAttributes;
  * 2.) XmlAttributeCollection xmlBptAttributeCollection field to store the collection of attributes of the xmlBprNode;
  * 3.) String bpId field to store string value of the id; [id value should be always integer in the case of bpt tags]
- * 4.) String bptcontent field to store native code contained between <bpt><\bpt> tags.
+ * 4.) String bptcontent field to store native code contained between <bpt><\bpt> tags. [Or <sub> elements which we will not model at the time]
  * 
  * Class for modelling should permit to:
  * 1.) Use Properties:
- *      1.1.) GetXmlNode to return the xmlTransUnitNode;
+ *      1.1.) GetXmlNode to return the xmlBptNode;
  *      1.2.) GetId to return string value of the "id" attribute;
- *      1.3.) GetTranslate to return string value of the "translatable" attribute.
+ *      1.3.) GetContent to return string value of the content contained between <bpt></bpt> tags.
  * 2.) Use Methods:
+ *      2.1.) GetAttributesCount() to return the int value that indicates the number of attributes contained in bpt XmlNode. Returns -1 if the list of XmlAttributes is null. 
+ *      2.2.) IsAttributeContained(string attributeName) to return the int value that indicates whether attirbute of a attributeName is contained in the XmnlAttribute list or not. Returns 1 if is, 0 if not and -1 if XmlAttributeList is empty or null. 
+ *      2.3.) GetXmlAttribute(string attributeName) to return XmlAttribute value for the given attributeName. If there is no attribute of that name in the XmlAttributeList (or the list is empty) method returns null. 
+ *      2.4.) GetXmLAttributeValue(string attributeName) to return string value of the attribute of the given name. This method works in a same way as GetXmlAttribute(string attributeName) but returns empty string if there is no attribute of a given name.
  * 3.) Use Constructors:
  *      1.1.) Empty constructor to set all fields to null;
- *      1.2.) Constructor to which XmlNode is passed with trans-unit node. 
+ *      1.2.) Constructor to which XmlNode is passed with bpt node. 
  *		
  * Class for modeling should not permit to:
  * 
@@ -80,6 +84,14 @@ namespace XLZ_Library.XLF.TransUnit.Languages.Elements
         public string bptContent;
 
         /* Properties */
+
+        public XmlNode GetXmlNode
+        {
+            get
+            {
+                return xmlBptNode;
+            }
+        }
 
         public string BptId
         {
